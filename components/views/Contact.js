0e0cd715da0921c69d62b42089f64b257c42f861
dpierson94 <dpierson94@gmail.com>
2022-12-12 15:46:17 -0600

    Week 6 Homework Submission

--- components/views/Contact.js
+++ components/views/Contact.js
@@ -4,7 +4,7 @@ export default () => html`
 <section id="contact">
 <form id="contact" method="POST" action="">
   <h2>Contact and Suggestions</h2>
-  <div>
+  <div id="suggestion">
     <label for="suggestion">Suggestions:</label><br>
     <input type ="text" id="suggestion" name="suggestion">
   </div>
--- components/views/Home.js
+++ components/views/Home.js
@@ -3,5 +3,5 @@ import html from "html-literal";
 export default () => html`
 <section id="Home">
   <h2>IS Report MO</h2>
-
+    <p>Welcome to Invasive Species(IS) Report MO.</p>
   `;
